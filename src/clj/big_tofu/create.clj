000604682ff7e9f6349a54df2ead4a959f525b2e
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-04-08 07:02:34 +0200

    refactor: useless [

--- src/clj/big_tofu/create.clj
+++ src/clj/big_tofu/create.clj
@@ -42,16 +42,16 @@
         assume-role (when (and assume-role
                                (not (str/blank? assume-role)))
                       {:assume_role {:role_arn assume-role}})]
-    {:provider {:aws [(merge {:region region}
-                             assume-role)]}
-     :terraform [{:backend {:s3 [(merge {:bucket bucket
-                                         :encrypt true
-                                         :key key
-                                         :region region}
-                                        assume-role)]}
-                  :required_providers [{:aws {:source "hashicorp/aws"
-                                              :version "~> 5.0"}}]
-                  :required_version ">= 1.8.0"}]}))
+    {:provider {:aws (merge {:region region}
+                            assume-role)}
+     :terraform {:backend {:s3 (merge {:bucket bucket
+                                       :encrypt true
+                                       :key key
+                                       :region region}
+                                      assume-role)}
+                 :required_providers {:aws {:source "hashicorp/aws"
+                                            :version "~> 5.0"}}
+                 :required_version ">= 1.8.0"}}))
 
 (comment
   (->> (kms :alpha/big-kms)
