36245568503bb246c387838e78cafdaa6ad7f98d
Alberto Miorin <109069886+alberto-of@users.noreply.github.com>
2025-07-30 03:28:13 +0200

    feat: add port-assigner and aws secrets-manager

--- src/clj/big_config/utils.clj
+++ src/clj/big_config/utils.clj
@@ -1,4 +1,9 @@
-(ns big-config.utils)
+(ns big-config.utils
+  (:require
+   [babashka.fs :as fs]
+   [cheshire.core :as json]
+   [com.grzm.awyeah.client.api :as aws]
+   [com.grzm.awyeah.credentials :refer [profile-credentials-provider]]))
 
 (defn deep-merge
   "Recursively merges maps."
@@ -21,3 +26,32 @@
     (or (vector? m)
         (seq? m)) (mapv sort-nested-map m)
     :else m))
+
+(defn invoke [client operation request]
+  (let [response (aws/invoke client {:op operation :request request})]
+    (if (contains? response :cognitect.anomalies/category)
+      (throw (ex-info "AWS operation failed" {:operation operation :request request :response response}))
+      response)))
+
+(defn secrets->map
+  [& {:keys [credentials-provider secret-id]}]
+  (let [client (aws/client {:api :secretsmanager
+                            :region "eu-west-1"
+                            :credentials-provider credentials-provider})]
+    (-> (invoke client :GetSecretValue {:SecretId secret-id
+                                        :Query :SecretString})
+        :SecretString
+        (json/decode true))))
+
+(defn port-assigner [service]
+  (-> (fs/cwd)
+      (str service)
+      hash
+      abs
+      (mod 64000)
+      (+ 1024)))
+
+(comment
+  (secrets->map :credentials-provider (profile-credentials-provider "data_lake_dacore")
+                :secret-id "arn:aws:secretsmanager:region:111111111111:secret:prod/squad/system-suffix")
+  (port-assigner ["postgres"]))
