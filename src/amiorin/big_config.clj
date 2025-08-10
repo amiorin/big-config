81aec53608e63a97564cd5e7f05cd400fdd5bac7
Alberto Miorin <109069886+alberto-of@users.noreply.github.com>
2025-08-10 21:17:12 +0200

    feat: use latest deps-new and big-config

--- src/amiorin/big_config.clj
+++ src/amiorin/big_config.clj
@@ -1,12 +1,28 @@
 (ns amiorin.big-config
   (:require
-   [babashka.process :as p]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [clojure.tools.deps]
+   [clojure.tools.deps.extensions :as extensions]))
+
+(defn do-find-latest-version
+  [lib-sym]
+  (let [procurer nil
+        types (distinct (into [:mvn :git] (extensions/procurer-types)))]
+    (->> (some #(extensions/find-versions lib-sym nil % procurer) types)
+         (remove #(str/ends-with? (:git/tag %) "-rc"))
+         last
+         (reduce-kv #(format "%s %s \"%s\"" %1 %2 %3) "")
+         str/trim
+         (format "{%s}"))))
 
 (defn data-fn
   [data]
-  (let [big-config-latest-rev (-> (p/shell {:out :string} "git ls-remote https://github.com/amiorin/big-config.git refs/heads/main")
-                                  :out
-                                  (str/split #"\s+")
-                                  first)]
-    (assoc data :big-config-latest-rev big-config-latest-rev)))
+  (->> (for [lib-sym ['io.github.amiorin/big-config
+                      'io.github.seancorfield/deps-new]]
+         {(keyword (str (name lib-sym) "-latest-version")) (do-find-latest-version lib-sym)})
+       (reduce #(merge %1 %2) data)))
+
+(comment
+  (data-fn {})
+  (do-find-latest-version 'io.github.seancorfield/deps-new)
+  (do-find-latest-version 'io.github.amiorin/big-config))
