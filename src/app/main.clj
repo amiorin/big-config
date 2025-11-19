f36f546b9108c5e4a65fc1bbaef2e4a3598b07d6
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-11-19 14:45:37 +0000

    feat: add aero and dynamic port

--- src/app/main.clj
+++ src/app/main.clj
@@ -228,6 +228,7 @@
 (defn -main [& _]
   (h/start-app
    {:max-refresh-ms 100
+    :port           (h/env :port)
     :ctx-start      ctx-start
     :ctx-stop       (fn [_state] nil)
     :csrf-secret    (h/env :csrf-secret)}))
