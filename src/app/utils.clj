ad0d4a69a8d0bc08e765920fb8c2ca0d262bcb0c
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-11-22 04:34:41 +0000

    refactoring: in multiple files

---
+++ src/app/utils.clj
@@ -0,0 +1,16 @@
+(ns app.utils)
+
+(defn encode [s]
+  (-> s
+      (str/replace "%" "%25")
+      (str/replace "'" "%27")
+      (str/replace "\"" "%22")))
+
+(defn decode [s]
+  (-> s
+      (str/replace "%25" "%")
+      (str/replace "%27" "'")
+      (str/replace "%22" "\"")))
+
+(comment
+  (-> "'%\"" encode decode))
